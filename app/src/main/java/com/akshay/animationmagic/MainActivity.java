0fdf709fff5ff1d2cb484f1aa827d744f1507ce0
AkshDanej <akshaydanej18@gamail.com>
2017-06-19 01:02:26 -0700

    Initial commit

---
+++ app/src/main/java/com/akshay/animationmagic/MainActivity.java
@@ -0,0 +1,17 @@
+package com.akshay.animationmagic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PlayGifView pGif = (PlayGifView) findViewById(R.id.viewGif);
+        pGif.setImageResource(R.drawable.newgif);
+
+    }
+}
